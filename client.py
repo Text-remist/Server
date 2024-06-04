39a08de51be32b692d747b48b4b185f1cf308adb
Text-remist <50calibursword@gmail.com>
2024-06-04 08:45:24 -0400

    Add files via upload

--- client.py
+++ client.py
@@ -8,7 +8,7 @@ HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "169.254.158.91"
+SERVER = input("SERVER IP: ")
 ADDR = (SERVER, PORT)
 global connected
 connected = False
@@ -20,29 +20,31 @@ def start():
     except ConnectionRefusedError:
         print("Server Down")
         connected = False
-
+    print("[CLIENT] CONNECTING TO SERVER")
+    print()
     def disconnect():
         global connected
         print()
-        print("[CLIENT] DISCONNECTING FROM SERVER")
-        print()
+        print("[CLIENT] DISCONNECTING FROM SERVER\n")
         # Assuming 'client' is the socket object used to connect to the server
         client.send(DISCONNECT_MESSAGE.encode(FORMAT))
         connected = False
 
     def gui():
-        r = tk.Tk()
-        r.title('Counting Seconds')
-        button = tk.Button(r, text='Disconnect From Server', width=25, command=lambda: [disconnect(), r.destroy()])
-        button.pack()
-        r.mainloop()
+        if connected == True:
+            r = tk.Tk()
+            r.title('Client Menu')
+            button = tk.Button(r, text='Disconnect From Server', width=25, command=lambda: [disconnect(), r.destroy()])
+            button.pack()
+            r.mainloop()
 
     def start_gui_thread():
         gui_thread = threading.Thread(target=gui)
         gui_thread.start()
 
     # Start the GUI thread
-    start_gui_thread()
+    if connected:
+        start_gui_thread()
     while connected:
 
         try:
@@ -51,20 +53,23 @@ def start():
             msg = client.recv(40000).decode(FORMAT)
             # Assuming msg includes both address and JSON data separated in some manner
             # Example format: "{address} {json_string}"
-
-            # Parsing the received JSON message
-            data = msg.replace("'", '"')
-            data = json.loads(data)
-            print("[CLIENT] Server Data Received")
-            data['age'] = random.randint(0,15)
-            data = json.dumps(data)
-            data = f"{data}"
-            client.send(data.encode(FORMAT))
-            print("[CLIENT] Server Data Sent")
+            if msg != DISCONNECT_MESSAGE:
+                # Parsing the received JSON message
+                data = msg.replace("'", '"')
+                data = json.loads(data)
+                print("[CLIENT] Server Data Received")
+                data['age'] = random.randint(0,15)
+                data = json.dumps(data)
+                data = f"{data}"
+                client.send(data.encode(FORMAT))
+                print("[CLIENT] Server Data Sent\n")
+            else:
+                connected = False
+                disconnect()
+                print("[CLIENT] REASON: BLOCKED")
+                return 0
         except json.JSONDecodeError as e:
             print(f"Failed to decode JSON: {e}")
-        except WindowsError as e:
-            print("Server closed")
             return 0
         except Exception as e:
             print(f"An error occurred: {e}")
--- server.py
+++ server.py
@@ -1,45 +1,63 @@
 import socket
 import threading
 import json
+
 data =  '{ "name":"John", "age":30, "city":"New York"}'
 json_data = json.loads(data)
 HEADER = 64
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
+BLOCKEDLST = json.load(open("blocked.json"))
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
-
+global running
 def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
-    print()
-
     connected = True
-    while connected:
-            data = f"{json_data}"
-            conn.send(data.encode(FORMAT))
-            msg = conn.recv(40000).decode(FORMAT)
-            if msg != DISCONNECT_MESSAGE:
-            # Assuming msg includes both address and JSON data separated in some manner
-            # Example format: "{address} {json_string}"
-
-            # Parsing the received JSON message
-                data = json.loads(msg)
-            else:
-                print(f"[SERVER] {addr} has disconnected")
+    blocked = False
+    for blocked_address in BLOCKEDLST:
+        if addr[0] == blocked_address:
+            blocked = True
+            break
+    if connected and not blocked:
+        print(f"[NEW CONNECTION] {addr}\n")
+        while connected:
+            try:
+                sent = f"{json_data}"
+                conn.send(sent.encode(FORMAT))
+                received = conn.recv(500000).decode(FORMAT)
+                if received != DISCONNECT_MESSAGE:
+                    received = json.loads(received)
+                else:
+                    print(f"[CLIENT] {addr} DISCONNECTED")
+                    connected = False
+            except WindowsError as e:
+                print("[CLIENT] HAS INCORRECTLY DISCONNECTED")
                 connected = False
-    conn.close()
+        conn.close()
+    else:
+        print(f"[SERVER] CLIENT ({addr[0]}) IS ON BLOCKLIST\n[SERVER] DISCONNECTING BLOCKED CLIENT")
+        conn.send(DISCONNECT_MESSAGE.encode(FORMAT))
+        connected = False
+        conn.close()
+def shutdown():
+    global running
+    print("\n[SERVER] DISCONNECTING CLIENT FROM SERVER\n")
+    running = False
+    server.close()  # Close the server socket
 
 def start():
+    running = True
     server.listen()
-    print(f"[LISTENING] Server is listening on {SERVER}")
-    while True:
+    print(f"[SERVER] SERVER RUNNING ON {SERVER}")
+    while running:
         conn, addr = server.accept()
 
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
         print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
-print("[STARTING] server is starting...")
+
+print("[SERVER] SERVER IS STARTING")
 start()
